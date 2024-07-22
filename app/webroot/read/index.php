1ae9b5066dc7e55bbb587600937018ed7739e18a
thiagonic <tmagalhaes@nic.br>
2024-07-22 12:10:32 -0300

    algumas alterações

--- app/webroot/read/index.php
+++ app/webroot/read/index.php
@@ -1,27 +1,26 @@
 <?php include '../functions.php'; ?>
-
 <!DOCTYPE html>
 <html>
 <head>
-    <title>To-Do List Items</title>
+    <title>Create New Item</title>
     <link rel="stylesheet" type="text/css" href="../style.css">
 </head>
 <body>
+<div class="view-item">
     <h1>To-Do List Items</h1>
-
-    <ul>
+    <ul class="todo-list-read">
         <?php
         $items = get_all_items();
         foreach ($items as $item) {
             echo "<li>";
-            echo "<h2>$item[name]</h2>";
+            echo "<h2>$item[name] - $item[concluded]</h2>";
             echo "<p>$item[description]</p>";
-            echo "<p>$item[concluded]</p>";
             echo "</li>";
         }
         ?>
+        <a href="../">Back to Index</a>
     </ul>
+</div>
 
-    <a href="../">Back to Index</a>
 </body>
 </html>
